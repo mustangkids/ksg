3da7fb8773ffcbddb9d4203e818321979a5f6a4e
mustangkids <39920226+mustangkids@users.noreply.github.com>
2023-01-31 23:43:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,34 @@ function updateImage() {
   var path = folder + images[randomIndex];
   document.getElementById("slider").src = path;
 }
+// Current slide index
+let currentSlide = 0;
+
+// Get the current slide index from local storage
+if (localStorage.getItem("currentSlide")) {
+  currentSlide = parseInt(localStorage.getItem("currentSlide"));
+}
+
+// Save the current slide index to local storage
+function saveSlide(index) {
+  localStorage.setItem("currentSlide", index);
+}
+
+// Example usage:
+
+// On page load
+document.addEventListener("DOMContentLoaded", function() {
+  showSlide(currentSlide);
+});
+
+// When moving to the next slide
+function nextSlide() {
+  saveSlide(currentSlide + 1);
+  showSlide(currentSlide + 1);
+}
+
+// When moving to the previous slide
+function prevSlide() {
+  saveSlide(currentSlide - 1);
+  showSlide(currentSlide - 1);
+}
