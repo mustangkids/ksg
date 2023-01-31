d5a8878d3f7114b94f4adc84a6256f16e443014d
mustangkids <39920226+mustangkids@users.noreply.github.com>
2023-01-31 14:38:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+var folder = "images/";
 var images = ["resim1.png", "resim2.png", "resim3.png"];
 var i = 0;
 
 function updateImage() {
-    document.getElementById("slider").src = "images/" + images[i];
-    if (i < images.length - 1) {
-        i++;
-    } else {
-        i = 0;
-    }
+  var path = folder + images[i];
+  document.getElementById("slider").src = path;
+  if (i < images.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
 }
