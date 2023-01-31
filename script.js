379f3c131b6385da1d2c37964a31e3bcdc725591
mustangkids <39920226+mustangkids@users.noreply.github.com>
2023-02-01 00:16:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
 var folder = "images/";
 var images = ["resim1.png", "resim2.png", "resim3.png","resim4.png","resim5.png","resim6.png","resim7.png"];
 
-function updateImage() {
-  var randomIndex = Math.floor(Math.random() * images.length);
-  var path = folder + images[randomIndex];
-  document.getElementById("slider").src = path;
-}
 // Current slide index
 let currentSlide = 0;
 
-// Get the current slide index from local storage
-if (localStorage.getItem("currentSlide")) {
-  currentSlide = parseInt(localStorage.getItem("currentSlide"));
+// Get the current slide index from a cookie
+if (getCookie("currentSlide")) {
+  currentSlide = parseInt(getCookie("currentSlide"));
 }
 
-// Save the current slide index to local storage
+// Save the current slide index to a cookie
 function saveSlide(index) {
-  localStorage.setItem("currentSlide", index);
+  setCookie("currentSlide", index, 7);
+}
+
+// Get the value of a cookie
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+// Set a cookie
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 // Example usage:
