ba30c1e8da90a36b8e970a3b86fd9c0b4cd12a8e
mustangkids <39920226+mustangkids@users.noreply.github.com>
2023-01-31 16:16:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var folder = "images/";
-var images = ["resim1.png", "resim2.png", "resim3.png"];
+var images = ["resim1.png", "resim2.png", "resim3.png","resim4.png","resim5.png","resim6.png","resim7.png"];
 
 function updateImage() {
   var randomIndex = Math.floor(Math.random() * images.length);
