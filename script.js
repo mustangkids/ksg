b46a4cb9e70a5e200e5cbc15445b3f2955cc164c
mustangkids <39920226+mustangkids@users.noreply.github.com>
2023-01-31 16:13:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 var folder = "images/";
 var images = ["resim1.png", "resim2.png", "resim3.png"];
-var i = 0;
 
 function updateImage() {
-  var path = folder + images[i];
+  var randomIndex = Math.floor(Math.random() * images.length);
+  var path = folder + images[randomIndex];
   document.getElementById("slider").src = path;
-  if (i < images.length - 1) {
-    i++;
-  } else {
-    i = 0;
-  }
 }
